1315d0233439962fcad81cc320573b97f9f06a14
guoziyue <2567047574@qq.com>
2022-06-03 11:34:40 +0800

    第二次提交

---
+++ src/main/java/com/msb/pojo/Person.java
@@ -0,0 +1,11 @@
+package com.msb.pojo;
+
+/**
+ * @Auther:Guo ziyue
+ * @Date: 2022/6/3 - 06 - 03 - 2022/6/3 - 11:33
+ * @Description: com.msb.pojo
+ * @Version: 1.0
+ */
+public class Person {
+    private Integer age;
+}
